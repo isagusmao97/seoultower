262242e40e49685426a5410d7eeb8d5143031824
isabelagusmao <isabelagusmao93@gmail.com>
2022-07-15 14:16:56 -0300

    changed text position

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,7 +6,7 @@
         <img src="../assets/imagens/nseoultower.jpg">
 
         <q-card-section>
-          <div class="text-h6">N Seoul Tower</div>
+          <div class="text-h6 text-center">N Seoul Tower</div>
           <div class="text-body2 text-weight-light text-justify" style="margin-top:5px">The N Seoul Tower officially called
           the CJ Seoul Tower and better known as 
           The Namsan Tower  or Seoul Tower, is a 
@@ -22,7 +22,7 @@
         <img src="../assets/imagens/nseoultower2.jpg">
 
         <q-card-section>
-          <div class="text-h6">History</div>
+          <div class="text-h6 text-center">History</div>
           <div class="text-body2 text-weight-light text-justify" style="margin-top:5px">It marks the highest point in Seoul.
         Built between 1969 and 1975, at a cost of 
         approximately  $ 2.5 million, it was opened
@@ -39,7 +39,7 @@
         <img src="../assets/imagens/nseoultower3.jpg">
 
         <q-card-section>
-          <div class="text-h6">Attractions</div>
+          <div class="text-h6 text-center">Attractions</div>
           <div class="text-body2 text-weight-light text-justify" style="margin-top:5px">Many visitors use the cable car to reach the 
         mountain and then complete the walk to the
         tower. The tower has a gift shop and restaurants 
@@ -59,11 +59,13 @@
     <q-card class="my-card01">
       <img src="../assets/imagens/foto05.jpg">
       <q-card-section>
-          <div class="text-h6">Love Locks</div>
+          <div class="text-h6 text-center">Love Locks</div>
           <div class="text-body2 text-weight-light text-justify" style="margin-top:5px">The Love Lock Bridge originated from tales about locking the padlocks and 
             throwing away the keys symbolizing eternal love. 
             But… the love locks do not limit to couples. 
             Families and friends lock their love for each other too!
+            It is a cliche, yet it's romantic. 
+            You can take pictures, chat, and enjoy the beautiful view of Seoul.
              </div>
         </q-card-section>
 
@@ -77,7 +79,7 @@
     <q-card class="my-card01">
       <img src="../assets/imagens/teddybear.jpg">
       <q-card-section>
-          <div class="text-h6">Teddy Bear Museum</div>
+          <div class="text-h6 text-center">Teddy Bear Museum</div>
           <div class="text-body2 text-weight-light text-justify" style="margin-top:5px ">The museum in N Seoul Tower narrates with teddy bears the past, present, 
             and future of Seoul. There is a history gallery and a special gallery where a teddy bear is dressed as a king of Joseon Dynasty 
             and also shows a wide variety of early images of Seoul in the old days.</div>
@@ -93,12 +95,11 @@
     <q-card class="my-card01">
       <img src="../assets/imagens/ngrill.jpg">
       <q-card-section>
-          <div class="text-h6">N Grill French Restaurant</div>
+          <div class="text-h6 text-center">N Grill French Restaurant</div>
           <div class="text-body2 text-weight-light text-justify" style="margin-top:5px ">High class French cuisine directly presented by n.Grill chef!
             n.Grill, a French restaurant, is located in the highest point of the N SEOUL TOWER
             boasting the best view.
-            Watching jewel-like night views, you can enjoy creative French courses presented
-            by n.Grill chef. It should be unforgettable memories for you and your love.</div>
+            It should be unforgettable memories for you and your love.</div>
         </q-card-section>
 
       <q-card-actions align="center">
@@ -134,6 +135,6 @@ export default defineComponent({
 .my-card01
   width: 100%
   max-width: 300px
-  height:550px
+  height:530px
   margin-top:35px
 </style>
